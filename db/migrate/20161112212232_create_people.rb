3dcaf123a18441d67179ccdcb4fcb1cf6a681802
Conor Starrs <conor.starrs@gmail.com>
2016-11-13 00:27:24 +0000

    Initial commit

---
+++ db/migrate/20161112212232_create_people.rb
@@ -0,0 +1,12 @@
+class CreatePeople < ActiveRecord::Migration[5.0]
+  def change
+    create_table :people do |t|
+      t.string :firstname
+      t.string :lastname
+      t.string :attending
+      t.string :bus
+
+      t.timestamps
+    end
+  end
+end
